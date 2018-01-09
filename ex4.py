3c68efd4e0b2f2713ee74831302edec5160d7798
varditA <vardit.arkash@gmail.com>
2018-01-09 20:02:21 +0200

    only imported package

---
+++ ex4.py
@@ -0,0 +1,30 @@
+import wikipedia, spacy
+# from spacy.token import Doc
+
+nlp_model = spacy.load('en')
+page = wikipedia.page('Brad Pitt').content
+analyzed_page = nlp_model(page)
+
+
+
+def extractor_by_pos():
+    pass
+
+
+def extractor_by_dependency_tree():
+    pass
+
+
+def evaluation():
+    pass
+
+
+def main():
+    print("hello world")
+    # extractor_by_pos()
+    # extractor_by_dependency_tree()
+    # evaluation()
+
+
+if __name__ == '__main__':
+    main()
