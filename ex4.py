e8d668296dd5d0459fb7c0ba372ae53dc355f3af
varditA <vardit.arkash@gmail.com>
2018-01-10 13:48:03 +0200

    finished part a

--- ex4.py
+++ ex4.py
@@ -4,12 +4,75 @@ import spacy
 # from spacy.token import Doc
 
 nlp_model = spacy.load('en')
-page = wikipedia.page('Brad Pitt').content
-analyzed_page = nlp_model(page)
 
 
-def extractor_by_pos():
-    pass
+def proper_nouns_for_sent(sent):
+    nouns = list(sent.noun_chunks)
+    if not nouns:
+        return set()
+    proper_nouns = set()
+    for noun in nouns:
+        to_add = True
+        for token in noun:
+            if token.pos_ != 'PROPN':
+                to_add = False
+                break
+        if to_add:
+            proper_nouns.add(noun)
+    return proper_nouns
+
+
+def pairs_for_sents(proper_nouns, doc):
+    triplet = []
+    for noun1 in proper_nouns:
+        for noun2 in proper_nouns:
+            # print(noun1, noun2)
+            ind1 = noun1[-1].i
+            ind2 = noun2[0].i
+
+            found_verb = False
+            found_punct = False
+
+            relations = list()
+
+            for i in range(ind1 + 1, ind2):
+                token = doc[i]
+                if token.pos_ == 'VERB':
+                    relations.append(token)
+                    found_verb = True
+                elif token.pos_ == 'ADP':
+                    relations.append(token)
+                elif token.pos_ == 'PUNCT':
+                    found_punct = True
+                    break
+            if not found_punct and found_verb:
+                triplet.append((noun1, relations, noun2))
+    return triplet
+
+
+def extractor_by_pos(document):
+    doc = nlp_model(document)
+    sents = list(doc.sents)
+    relations_dict = {}
+
+    for i, sent in enumerate(sents):
+        # PART A
+        proper_nouns = proper_nouns_for_sent(sent)
+
+        # Need at least 2 proper nouns to create pairs
+        if len(proper_nouns) <= 1:
+            continue
+
+        # PART B
+        triplet = pairs_for_sents(proper_nouns, doc)
+
+        # Need at least 1 pair to create relations
+        if len(triplet) < 1:
+            continue
+
+        relations_dict[i] = triplet
+
+    return relations_dict
 
 
 def extractor_by_dependency_tree():
@@ -21,8 +84,10 @@ def evaluation():
 
 
 def main():
-    print("hello world")
-    # extractor_by_pos()
+    page = wikipedia.page('Brad Pitt').content
+    dict_by_pos = extractor_by_pos(document=page)
+    print(dict_by_pos)
+
     # extractor_by_dependency_tree()
     # evaluation()
 
