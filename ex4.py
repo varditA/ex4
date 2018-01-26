da731b86ac6ff49c737d34c0656b03adbe192756
varditA <vardit.arkash@gmail.com>
2018-01-26 15:42:21 +0200

    need to sort

--- ex4.py
+++ ex4.py
@@ -1,14 +1,15 @@
 import wikipedia
 import spacy
 import random
+
 # from spacy.token import Doc
 
 nlp_model = spacy.load('en')
 
 
 # ----------------------------- Part A ---------------------------------------
-def proper_nouns_for_sent_by_pos(sent):
-    tokens = list(sent)
+def proper_nouns_for_sent_by_pos(doc):
+    tokens = list(doc)
     proper_nouns = []
     curr_set = []
     for token in tokens:
@@ -48,72 +49,73 @@ def pairs_for_sents(proper_nouns, doc):
 
 
 # def pairs_for_sents(proper_nouns, doc):
-    # triplet = []
-    # for i in range(len(proper_nouns) - 1):
-    #     noun1 = proper_nouns[i]
-    #     noun2 = proper_nouns[i+1]
-    #
-    #     ind1 = noun1[-1].i
-    #     ind2 = noun2[0].i
-    #
-    #     found_verb = False
-    #     found_punct = False
-    #
-    #     relations = list()
-    #
-    #     for i in range(ind1 + 1, ind2):
-    #         token = doc[i]
-    #         if token.pos_ == 'VERB':
-    #             relations.append(token)
-    #             found_verb = True
-    #         elif token.pos_ == 'ADP':
-    #             relations.append(token)
-    #         elif token.pos_ == 'PUNCT':
-    #             found_punct = True
-    #             break
-    #     if not found_punct and found_verb:
-    #         triplet.append((noun1, relations, noun2))
-    # return triplet
+#     triplet = []
+#     for i in range(len(proper_nouns) - 1):
+#         noun1 = proper_nouns[i]
+#         noun2 = proper_nouns[i+1]
+#
+#         ind1 = noun1[-1].i
+#         ind2 = noun2[0].i
+#
+#         found_verb = False
+#         found_punct = False
+#
+#         relations = list()
+#
+#         for i in range(ind1 + 1, ind2):
+#             token = doc[i]
+#             if token.pos_ == 'VERB':
+#                 relations.append(token)
+#                 found_verb = True
+#             elif token.pos_ == 'ADP':
+#                 relations.append(token)
+#             elif token.pos_ == 'PUNCT':
+#                 found_punct = True
+#                 break
+#         if not found_punct and found_verb:
+#             triplet.append((noun1, relations, noun2))
+#     return triplet
 
 
 def extractor_by_pos(document):
     doc = nlp_model(document)
-    sents = list(doc.sents)
     relations_list = []
 
-    for i, sent in enumerate(sents):
-        # PART A
-        proper_nouns = proper_nouns_for_sent_by_pos(sent)
+    # PART A
+    proper_nouns = proper_nouns_for_sent_by_pos(doc)
 
-        # Need at least 2 proper nouns to create pairs
-        if len(proper_nouns) <= 1:
-            continue
+    # Need at least 2 proper nouns to create pairs
+    if len(proper_nouns) <= 1:
+        return []
 
-        # PART B
-        triplet = pairs_for_sents(proper_nouns, doc)
+    # PART B
+    triplet = pairs_for_sents(proper_nouns, doc)
 
-        # Need at least 1 pair to create relations
-        if len(triplet) < 1:
-            continue
+    # Need at least 1 pair to create relations
+    if len(triplet) < 1:
+        return []
 
-        for relation in triplet:
-            relations_list.append(relation)
+    for relation in triplet:
+        relations_list.append(relation)
 
     return relations_list
 
 
 # ----------------------------- Part B ---------------------------------------
 
-def proper_nouns_for_sent_by_tree(sent):
+def proper_nouns_for_sent_by_tree(doc):
+
+    print(doc)
     proper_nouns = []
-    tokens = list(sent)
+    tokens = list(doc)
     for token in tokens:
-        # print(token, token.pos_, token.dep_)
         if token.pos_ == 'PROPN' and token.dep_ != 'compound':
             token_list = [token]
             for child in token.children:
                 if child.dep_ == 'compound':
                     token_list.append(child)
+            sorted(token_list, key=lambda token_in: token_in.i)
+            print(token_list)
             proper_nouns.append(token_list)
 
     return proper_nouns
@@ -156,28 +158,43 @@ def define_relations(proper_nouns):
 
 def extractor_by_dependency_tree(document):
     doc = nlp_model(document)
-    sents = list(doc.sents)
+    # print(doc.text)
+    # sents = list(doc.sents)
     relations_list = []
 
-    for i, sent in enumerate(sents):
-        # print(sent)
-        proper_nouns = proper_nouns_for_sent_by_tree(sent)
-        # print(proper_nouns)
+    proper_nouns = proper_nouns_for_sent_by_tree(doc)
 
-        # Need at least 2 proper nouns to create pairs
-        if len(proper_nouns) <= 1:
-            continue
+    # Need at least 2 proper nouns to create pairs
+    if len(proper_nouns) <= 1:
+        return []
 
-        triplet = define_relations(proper_nouns)
-        # print(triplet)
-        # Need at least 1 pair to create relations
-        if len(triplet) < 1:
-            continue
+    triplet = define_relations(proper_nouns)
+    # Need at least 1 pair to create relations
+    if len(triplet) < 1:
+        return []
 
-        for relation in triplet:
-            relations_list.append(relation)
+    for relation in triplet:
+        relations_list.append(relation)
 
     return relations_list
+
+    # for i, sent in enumerate(sents):
+    #     proper_nouns = proper_nouns_for_sent_by_tree(sent)
+    #
+    #     # Need at least 2 proper nouns to create pairs
+    #     if len(proper_nouns) <= 1:
+    #         continue
+    #
+    #     triplet = define_relations(proper_nouns)
+    #     # print(triplet)
+    #     # Need at least 1 pair to create relations
+    #     if len(triplet) < 1:
+    #         continue
+    #
+    #     for relation in triplet:
+    #         relations_list.append(relation)
+    #
+    # return relations_list
 
 
 # ----------------------------- Part C ---------------------------------------
@@ -188,21 +205,24 @@ def evaluation():
     page_trump = wikipedia.page('Donald Trump').content
     page_jolie = wikipedia.page('Angelina Jolie').content
 
-    pages = [page_pitt, page_trump, page_jolie]
+    # pages = [page_trump, page_pitt, page_jolie]
+    pages = [page_trump]
+
 
     # evaluation for pos
     for page in pages:
         pos_list = extractor_by_pos(document=page)
+        print(len(pos_list))
         random.shuffle(pos_list)
         # print(pos_list[:30])
-        print(len(pos_list))
-        # print(pos_list)
 
     # evaluation for tree
-    # for page in pages:
-    #     tree_list = extractor_by_dependency_tree(document=page)
-    #     random.shuffle(tree_list)
-    #     print(len(tree_list))
+    for page in pages:
+        tree_list = extractor_by_dependency_tree(document=page)
+        print(len(tree_list))
+        for sentence in tree_list:
+            print(sentence)
+        random.shuffle(tree_list)
     #     # print(tree_list[:30])
 
 
